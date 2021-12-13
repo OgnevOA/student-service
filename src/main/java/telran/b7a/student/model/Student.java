6a418f6f2084ebfdf452707be3121c5ed9939445
Guago <Guago@DESKTOP-QUFV8SI.mshome.net>
2021-12-13 11:51:41 +0200

    Added MongoDB

--- src/main/java/telran/b7a/student/model/Student.java
+++ src/main/java/telran/b7a/student/model/Student.java
@@ -3,6 +3,9 @@ package telran.b7a.student.model;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +14,9 @@ import lombok.Setter;
 @Getter
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
+@Document(collection = "tipesh")
 public class Student {
+	@Id
 	int id;
 	@Setter
 	String name;
